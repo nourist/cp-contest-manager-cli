f333ce69b1c26503e9446075ffa9c0f4620d75c6
Nourist <hodinhvys@gmail.com>
2025-07-30 22:30:10 +0700

    chore: edit template

--- src/template/cpp.js
+++ src/template/cpp.js
@@ -1,6 +1,8 @@
 const template = `#include <bits/stdc++.h>
 using namespace std;
 
+#define FOR(i,l,r) for (int i = l; i <= r; ++i)
+#define FOD(i,r,l) for (int i = r; i >= l; --i)
 #define ll long long
 #define ld long double
 
